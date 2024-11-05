a1149b6ab0c18d84fc827c739728eaba8a84e101
Harlod Bombala <h.bombala@student.avans.nl>
2024-11-05 16:32:13 +0000

    project initial

---
+++ beestje-op-je-feestje/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace beestje_op_je_feestje.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
